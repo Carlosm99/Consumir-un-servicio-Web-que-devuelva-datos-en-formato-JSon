aa4d13e557da630ab8860fd29380b08459ad6b32
Carlosm99 <carlosmontess07@gmail.com>
2021-05-14 04:20:51 -0500

    async function, llame al servicio API y cree interface para usario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'consumirjson';
+export class AppComponent implements OnInit {
+
+  paises: any;
+
+  constructor( private dataServices: DataService) {}
+
+  ngOnInit(){
+
+   this.paises = this.dataServices.getCountries();
+  //   .subscribe( countries => {
+  //       console.log(countries);
+  //   } );
+  // }
+  // title = 'consumirjson';
+}
 }
